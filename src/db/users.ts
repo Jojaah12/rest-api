b01975822425633623b3caed73b041298fc84f71
Jojaah12 <david.aljojah@gmail.com>
2023-03-11 21:42:35 +0100

    finsihing up user model and schema

--- src/db/users.ts
+++ src/db/users.ts
@@ -5,5 +5,26 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true },
   authentication: {
     password: { type: String, required: true, select: false },
+    salt: { type: String, select: false },
+    sessionToken: { type: String, select: false },
   },
 });
+
+export const UserModel = mongoose.model('User', UserSchema);
+
+export const getUser = () => UserModel.findOne();
+export const getUserByEmail = (email: string) =>
+  UserModel.findOne({ email });
+export const getUserBySessionToken = (sessionToken: string) =>
+  UserModel.findOne({
+    'authentication.sessionToken': sessionToken,
+  });
+export const getUserById = (id: string) => UserModel.findOne(id);
+export const createUser = (values: Record<string, any>) =>
+  new UserModel(values).save().then((user) => user.toObject());
+export const deleteUserById = (id: string) =>
+  UserModel.findOneAndDelete({ _id: id });
+export const updateUserById = (
+  id: string,
+  values: Record<string, any>
+) => UserModel.findByIdAndUpdate(id, values);
